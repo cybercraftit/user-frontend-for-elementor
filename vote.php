142ba3383e4f0860588a781396066d444c55868d
Mithu A Quayium <cemithu06@gmail.com>
2020-09-07 07:19:39 +0600

    vote correction

--- vote.php
+++ vote.php
@@ -7,6 +7,7 @@ class UFEL_Vote {
     static $plugin_label = 'User Frontend for Elementor';
     static $fb_title = 'User Frontend for Elementor - must have WordPress plugin #UserFrontendforElementor';
     static $tw_title = 'User Frontend for Elementor - must have WordPress plugin #UserFrontendforElementor';
+    static $plugin_wp_url = 'https://wordpress.org/plugins/user-frontend-for-elementor/';
 
     public function __construct() {
         add_action( 'init', array( __CLASS__, 'vote_init' ) );
@@ -52,10 +53,10 @@ class UFEL_Vote {
                 $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=yes" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button button button-small button-primary" data-action="http://wordpress.org/support/view/plugin-reviews/'. self::$plugin_name .'?rate=5#postform">'.__( 'Rate us', 'sm' ).'</a>';
             }
             if( !isset( $ufel_votes['tweet'] ) ) {
-                $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=tweet" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button button button-small" data-action="http://twitter.com/share?url=http://bit.ly/2mXDU5t&amp;text='.urlencode( __( self::$tw_title, 'sm' ) ).'">'.__( 'Tweet', 'sm' ).'</a>';
+                $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=tweet" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button button button-small" data-action="http://twitter.com/share?url='.self::$plugin_wp_url.'&amp;text='.urlencode( __( self::$tw_title, 'sm' ) ).'">'.__( 'Tweet', 'sm' ).'</a>';
             }
             if( !isset( $ufel_votes['facebook'] ) ) {
-                $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=facebook" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button button button-small" data-action="http://facebook.com/sharer?u=http://bit.ly/2mXDU5t&amp;text='.urlencode( __( self::$fb_title, 'sm' ) ).'">'.__( 'Share on facebook', 'sm' ).'</a>';
+                $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=facebook" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button button button-small" data-action="http://facebook.com/sharer?u='.self::$plugin_wp_url.'&amp;text='.urlencode( __( self::$fb_title, 'sm' ) ).'">'.__( 'Share on facebook', 'sm' ).'</a>';
             }
             if( !isset( $ufel_votes['no'] ) ) {
                 $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=no" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button '.self::$text_domain.'-cancel-button button button-small">'.__( 'No, thanks', 'sm' ).'</a>';
@@ -66,7 +67,6 @@ class UFEL_Vote {
             if( !isset( $ufel_votes['later'] ) ) {
                 $btn_str .= '<a href="'.admin_url( 'admin-ajax.php' ).'?action=ufel_vote&amp;vote=later" class="'.self::$text_domain.'-vote-action '.self::$text_domain.'-vote-button button button-small">'.__( 'Remind me later', 'sm' ).'</a>';
             }
-
         }
 
 
